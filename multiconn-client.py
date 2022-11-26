eaa4fd8094db73f3246ea0095efea2ae196fe3f3
yeongbin <baack7700@hanyang.ac.kr>
2022-11-26 22:23:56 +0900

    change the code cleanly

--- multiconn-client.py
+++ multiconn-client.py
@@ -39,12 +39,13 @@ def connection(ip_data, port):
             sock.connect(server_addr)
         except:
             print("Warning!!")
+            print(f"Fail to connect {server_addr}")
             continue
         socket_info = connect_id , sock
         socket_list.append(socket_info)
     return socket_list
 
-def start_connections(messages, id, sock_list):
+def put_message(messages, id, sock_list):
     # for i in range(len(messages["id"])):
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     to_list = []
@@ -55,11 +56,9 @@ def start_connections(messages, id, sock_list):
     soc_num = 100
     for j in range(len(sock_list)):
         if id == str(sock_list[j][0]):
-        # if messages["id"][i] == str(sock_list[j][0]):
             soc_num = j 
     if soc_num == 100:
         return 0
-    print("hello")
     connid = sock_list[soc_num][0]
     sock = sock_list[soc_num][1]
     for to in to_list:
@@ -72,7 +71,6 @@ def start_connections(messages, id, sock_list):
         outb=b"",
     )
     sel.register(sock, events, data=data)
-    print("끝")
     return 1
 
 def service_connection(key, mask):
@@ -83,29 +81,19 @@ def service_connection(key, mask):
         if recv_data:
             print(f"Received {recv_data!r} from connection {data.connid}")
             time.sleep(0.1)
-            print()
             data.recv_total += len(recv_data)
-            print("1!!")
             sel.unregister(sock)
-            return 1
         if not recv_data or data.recv_total == data.msg_total:
             print(f"Closing connection {data.connid}")
             sel.unregister(sock)
-            print("2!!")
-            return 1
     if mask & selectors.EVENT_WRITE:
         if not data.outb and data.messages:
             data.outb = data.messages.pop(0)
-            print("3!!")
-            return 1
         if data.outb:
             print(f"Sending {data.outb!r} to connection {data.connid}")
             sent = sock.send(data.outb)  # Should be ready to write
             time.sleep(0.1)
             data.outb = data.outb[sent:]
-            print("4!!")
-            return 1
-    return 0
 
 
 if len(sys.argv) != 3:
@@ -117,14 +105,13 @@ if len(sys.argv) != 3:
 ip_data = pd.read_csv(sys.argv[1])
 port = 65432
 sock_list = connection(ip_data,port)
-# start_connections(messages=messages, sock_list=sock_list)
+# put_message(messages=messages, sock_list=sock_list)
 
 try:
         # events = sel.select(timeout=1)
         for i in range(len(messages["id"])):
-            start_connections(messages=messages["command"][i], id= messages["id"][i], sock_list=sock_list)
+            put_message(messages=messages["command"][i], id= messages["id"][i], sock_list=sock_list)
             while True:
-            # start_connections(messages=messages["command"][i], id= messages["id"][i], sock_list=sock_list)
                 events = sel.select(timeout=1)
                 if events:
                     for key, mask in events:
